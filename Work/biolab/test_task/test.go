166f9a28f2df6779b3ca4bbe583f3d3324e42b17
Pavel Litvinov <plitvinov99@gmail.com>
2020-02-28 12:52:15 +0300

    work with map

--- Work/biolab/test_task/test.go
+++ Work/biolab/test_task/test.go
@@ -1,55 +1,109 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/xml"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main() {
-	type Email struct {
-		Where string `xml:"where,attr"`
-		Addr  string
-	}
-	type Address struct {
-		City, State string
-	}
-	type Result struct {
-		XMLName xml.Name `xml:"Person"`
-		Name    string   `xml:"FullName"`
-		Phone   string
-		Email   []Email
-		Groups  []string `xml:"Group>Value"`
-		Address
-	}
-	v := Result{Name: "none", Phone: "none"}
+//type Molecul struct {
+//	params map[string]string
+//}
 
-	data := `
-		<Person>
-			<FullName>Grace R. Emlin</FullName>
-			<Company>Example Inc.</Company>
-			<Email where="home">
-				<Addr>gre@example.com</Addr>
-			</Email>
-			<Email where='work'>
-				<Addr>gre@work.com</Addr>
-			</Email>
-			<Group>
-				<Value>Friends</Value>
-				<Value>Squash</Value>
-			</Group>
-			<City>Hanga Roa</City>
-			<State>Easter Island</State>
-		</Person>
-	`
-	err := xml.Unmarshal([]byte(data), &v)
+func checkError(message string, err error) {
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		log.Fatal(message, err)
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+func main() {
+
+	configurets := []string{"iupac_name", "average_molecular_weight", "status"}
+
+	xmlFile, err := os.Open("serum_metabolites.xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
 		return
 	}
-	fmt.Printf("XMLName: %#v\n", v.XMLName)
-	fmt.Printf("Name: %q\n", v.Name)
-	fmt.Printf("Phone: %q\n", v.Phone)
-	fmt.Printf("Email: %v\n", v.Email)
-	fmt.Printf("Groups: %v\n", v.Groups)
-	fmt.Printf("Address: %v\n", v.Address)
+	defer xmlFile.Close()
+
+	decoder := xml.NewDecoder(xmlFile)
+
+	//result:= new ([]map[string]string)
+	//current := make(map[string]string)
+	//var current  []string
+	//var result [][]string
+	var result_map []map[string]string
+	result_map_length := 0
+	for {
+		//len := 0
+		tok, tokenErr := decoder.Token()
+		if tokenErr != nil && tokenErr != io.EOF {
+			fmt.Println("error happend", tokenErr)
+			break
+		} else if tokenErr == io.EOF {
+			break
+		}
+
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			if tok.Name.Local == "metabolite" {
+				cs := make(map[string]string)
+				result_map = append(result_map, cs)
+				result_map_length++
+			}
+			if stringInSlice(tok.Name.Local, configurets) {
+				var buf string
+				if err := decoder.DecodeElement(&buf, &tok); err != nil {
+					fmt.Println("error", err)
+				}
+				result_map[result_map_length-1][tok.Name.Local] = buf
+				//fmt.Println(buf)
+				//current = append(current,buf)
+
+				//fmt.Println(len, result[len])
+
+			}
+			//case xml.EndElement:
+			//	if tok.Name.Local == "metabolite" {
+			//		result=append(result,current)
+			//	}
+			//}
+		}
+	}
+	//fmt.Println(result_map)
+	//for _, i := range result_map{
+	//	fmt.Println(i)
+	//}
+
+	file, err := os.Create("result.csv")
+	checkError("Cannot create file", err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.Write(configurets)
+
+	checkError("Cannot write to file", err)
+	for _, current := range result_map {
+		var text []string
+		for _, value := range configurets {
+			text = append(text, current[value])
+		}
+		fmt.Println(current)
+		err := writer.Write(text)
+		checkError("Cannot write to file", err)
+	}
 }
